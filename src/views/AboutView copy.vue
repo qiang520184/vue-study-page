2cb3eba8b26b59a8789748a9e631df7fc0f21b04
fudaqiang <fudaqiang@up366.com>
2023-08-11 14:23:54 +0800

    feat:添加主题

---
+++ src/views/AboutView copy.vue
@@ -0,0 +1,91 @@
+<template>
+    <codemirror
+      v-model="code"
+      placeholder="Code gose here..."
+      :style="{ height: '100%' }"
+      :autofocus="true"
+      :indent-with-tab="true"
+      :tabSize="2"
+      :extensions="extensions"
+      @ready="log('ready', $event)"
+      @change="log('change', $event)"
+      @focus="log('focus', $event)"
+      @blur="log('blur', $event)"
+    />
+  </template>
+  
+  <script>
+  import { Codemirror } from "vue-codemirror";
+
+  
+// 引入解析器包
+import { loadWASM } from 'onigasm'
+import { onMounted, ref } from 'vue'
+import { Registry } from 'monaco-textmate'
+import { wireTmGrammars } from 'monaco-editor-textmate'
+
+import { javascript } from '@codemirror/lang-javascript'
+import { css } from "@codemirror/lang-css";
+import { html } from "@codemirror/lang-html";
+import { json } from "@codemirror/lang-json";
+import { markdown } from "@codemirror/lang-markdown";
+import { oneDark } from '@codemirror/theme-one-dark'
+
+
+  export default {
+    components: {
+      Codemirror,
+    },
+    setup() {
+      const code = `<template>
+    <codemirror
+      v-model="code"
+      placeholder="Code gose here..."
+      :style="{ height: '400px' }"
+      :autofocus="true"
+      :indent-with-tab="true"
+      :tabSize="2"
+      :extensions="extensions"
+      @ready="log('ready', $event)"
+      @change="log('change', $event)"
+      @focus="log('focus', $event)"
+      @blur="log('blur', $event)"
+    />
+  </template>
+    
+    import { Codemirror } from "vue-codemirror";
+    // 引入解析器包
+    import { loadWASM } from 'onigasm'
+    import { onMounted, ref } from 'vue'
+    import { Registry } from 'monaco-textmate'
+    import { wireTmGrammars } from 'monaco-editor-textmate'
+    import { javascript } from '@codemirror/lang-javascript'
+    import { css } from "@codemirror/lang-css";
+    import { html } from "@codemirror/lang-html";
+    import { json } from "@codemirror/lang-json";
+    import { markdown } from "@codemirror/lang-markdown";
+    import { oneDark } from '@codemirror/theme-one-dark'
+    export default {
+        components: {
+        Codemirror,
+        },
+        setup() {
+        const extensions = [javascript(), oneDark];
+    
+        return {
+            code,
+            extensions,
+            log: console.log,
+        };
+        },
+    }`;
+      const extensions = [javascript(), oneDark];
+  
+      return {
+        code,
+        extensions,
+        log: console.log,
+      };
+    },
+  };
+  </script>
---
+++ themes1.js
@@ -0,0 +1,7 @@
+// const converter = require('vscode-theme-to-monaco-theme-node');
+
+// // 转换单个文件
+// converter.convertThemeFromFilePath(path.resolve(__dirname, 'themesFrom', 'OneDarkPro.jsonc'), path.resolve(__dirname, 'themesTo', 'OneDarkPro.json'))
+
+// // 转换多个文件
+// converter.convertThemeFromDir(path.resolve(__dirname, 'themesFrom'), path.resolve(__dirname, 'themesTo'))
